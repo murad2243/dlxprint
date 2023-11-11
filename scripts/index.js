e7b956f122c9193db8e2e10a71f779b31c7f9ddd
murad2234 <muradkodalwad2003@gmail.com>
2023-11-11 19:28:41 +0530

    navbar

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+    
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+    
+            everyitem.addEventListener('mouseover', function(e){
+    
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+    
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+    
+    
+            })
+        });
+    
+    }
+    // end if innerWidth
+    }); 
+function handleNavImage(event,url){
+    // console.log(event.target.innerText)
+    let img = document.getElementById("nav-img")
+    img.src= url;
+
+    let imgtext = document.getElementById("nav-img-text")
+    imgtext.innerText = event.target.innerText
+
+    console.log(url)
+}
