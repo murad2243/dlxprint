7606dccdcc4bc1e9bb32d72311737ab9055fe4b8
murad2234 <muradkodalwad2003@gmail.com>
2023-11-16 14:18:33 +0530

    landing page draft

--- scripts/index.js
+++ scripts/index.js
@@ -60,15 +60,17 @@ function handleSearch(search){
 
 // owl carousel 
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+// var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity( '.main-carousel', {
+//   // options
+// });
+
+// slick carousel 
